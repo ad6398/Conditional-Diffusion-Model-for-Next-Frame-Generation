52bba0f454978c417f50521cb490ca5c45a1c9fe
Amardeep <kumaramardipsingh@gmail.com>
2023-12-03 11:58:28 -0500

    remove config mod from cmd

--- src/mcvd/train.sh
+++ src/mcvd/train.sh
@@ -8,7 +8,7 @@ export data="${DL_PROJ}raw-data-1/dataset"
 
 # Video prediction non-spade
 export exp="${DL_PROJ}semantic_pred_5v5-debug-1"
-export config_mod="sampling.subsample=100 sampling.clip_before=True sampling.max_data_iter=1 model.version=DDPM model.arch=unetmore model.num_res_blocks=2"
+export config_mod=""
 
 
 CUDA_VISIBLE_DEVICES=0 python main.py --config configs/${config}.yml --data_path ${data} --exp ${exp} --ni --config_mod ${config_mod}
